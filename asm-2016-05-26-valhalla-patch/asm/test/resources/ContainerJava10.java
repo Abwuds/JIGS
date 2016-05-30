48984e2c0435475dac4668376318ddf52effb545
Abwuds <darealbaxter@gmail.com>
2016-05-30 16:29:49 +0200

    [Upd] Old java 10 example file.

--- asm-2016-05-26-valhalla-patch/asm/test/resources/ContainerJava10.java
+++ asm-2016-05-26-valhalla-patch/asm/test/resources/ContainerJava10.java
@@ -1,4 +1,3 @@
-package fr.upem.any;
 
 /**
  * Created by Baxtalou on 19/05/2016.
@@ -8,10 +7,6 @@ public class ContainerJava10<any T> {
 
     public ContainerJava10(T v) {
         this.v = v;
-    }
-
-    public ContainerJava10<T> clone() {
-        return new ContainerJava10<T>(v);
     }
 
     public T getV() {
