0d3a73a53f2025eed76979682ebcd56617938025
Abwuds <darealbaxter@gmail.com>
2016-09-15 14:39:16 +0200

    [Rfc] RT class refactored and documented. 1/2

--- asm-2016-05-26-valhalla-patch/backport-v0.1/src/specialization/Rewriter.java
+++ asm-2016-05-26-valhalla-patch/backport-v0.1/src/specialization/Rewriter.java
@@ -10,17 +10,19 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
- *
  * Created by Jefferson Mangue on 26/05/2016.
  */
 public class Rewriter {
 
     private static final String FOLDER_SPECIALIZATION = "compiled/";
-    /** The directory visited */
+    /**
+     * The directory visited
+     */
     private final String directory;
 
     /**
@@ -32,8 +34,8 @@ public class Rewriter {
 
     /**
      * Compiles all the classes present inside the working directory.
-     * @throws IOException
-     * TODO walk recursively.
+     *
+     * @throws IOException TODO walk recursively.
      */
     public void compileDirectory() throws IOException {
         File[] files = new File(directory).listFiles(new FilenameFilter() {
@@ -49,21 +51,23 @@ public class Rewriter {
 
     /**
      * Compile the class at the given path according to the rewriting rules.
+     *
      * @param path the path of the file to compileDirectory.
      * @throws IOException
      */
     private void compileClazz(final Path path) throws IOException {
         System.out.println("Writing class : " + path);
-        writeClazz(path.getFileName().toString(), dump(path));
+        writeClazzTo(path.getFileName().toString(), dump(path));
     }
 
     /**
      * Writes the given byte array to the specified path.
-     * @param path the path to write the clazz.
+     *
+     * @param path  the path to write the clazz.
      * @param clazz the byte array representing the clazz.
      * @throws IOException
      */
-    private void writeClazz(final String path, final byte[] clazz) throws IOException {
+    private void writeClazzTo(final String path, final byte[] clazz) throws IOException {
         File f = new File(new File(directory), FOLDER_SPECIALIZATION + path);
         if (!f.getParentFile().exists() && !f.getParentFile().mkdirs())
             throw new IOException("Cannot create directory " + f.getParentFile());
@@ -91,30 +95,50 @@ public class Rewriter {
 
     private void writeBackFactoryClazz(FrontClassVisitor frontClassVisitor) throws IOException {
         if (frontClassVisitor.hasBackFactory()) {
-            writeClazz(frontClassVisitor.getBackFactoryName() + ".class", frontClassVisitor.getBackFactoryBytes());
+            writeClazzTo(frontClassVisitor.getBackFactoryName() + ".class", frontClassVisitor.getBackFactoryBytes());
         }
     }
 
-    public static void main(final String[] args) throws IOException {
+    public static void main(final String[] args) throws IOException, URISyntaxException {
         String dir = args[0];
-        if (args.length < 0 || dir.isEmpty()) {
-            throw new IllegalArgumentException("Please provide the directory to visit.");
+        // ${folder} is the folder parameter not set when launching from the ant script.
+        if (args.length < 0 || dir.isEmpty() || dir.equals("${folder}")) {
+            throw new IllegalArgumentException("Please provide the directory to rewrite.");
         }
         Rewriter rewriter = new Rewriter(dir);
         rewriter.compileDirectory();
-        rewriter.copyRTClazz(ClassLoader.getSystemClassLoader().getResource("").getPath() + RT.class.getPackage().getName().replace('.', '/'));
+        rewriter.copyRTClazz(computeRTPackageAbsolutePath());
+        System.out.println("End");
     }
 
     private void copyRTClazz(String dir) throws IOException {
+        System.out.println(dir);
         File[] files = new File(dir).listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File d, String name) {
                 return name.endsWith(".class");
             }
         });
+        if (files == null) {
+            throw new IllegalStateException("Please provide the rt folder next to the Backport jar file. In the directory : " + dir);
+        }
         for (File f : files) {
             byte[] bytes = Files.readAllBytes(f.toPath());
-            writeClazz("rt/" + f.getName(), bytes);
+            writeClazzTo("rt/" + f.getName(), bytes);
         }
+    }
+
+    private static String computeRTPackageAbsolutePath() throws URISyntaxException {
+        String path = Rewriter.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+        String[] split = path.split("/");
+        StringBuilder sb = new StringBuilder("/");
+        String sep = "";
+        for (int i = 0; i < split.length - 1; i++) {
+            String s = split[i];
+            sb.append(s).append(sep);
+            sep = "/";
+        }
+        path = sb.toString();
+        return path + RT.class.getPackage().getName().replace('.', '/') + "/";
     }
 }
