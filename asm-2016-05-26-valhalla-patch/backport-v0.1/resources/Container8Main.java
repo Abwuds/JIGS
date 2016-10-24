f1c03a3173fb3e27dbd5937e612118127dffe89c
Abwuds <darealbaxter@gmail.com>
2016-10-24 18:07:11 +0200

    [Del] Some useless files.

--- asm-2016-05-26-valhalla-patch/backport-v0.1/resources/Container8Main.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * Created by Baxtalou on 19/05/2016.
- */
-public class Container8Main {
-    public static void main(String[]args){
-        Runnable r = new Runnable() { @Override public void run() {} };
-    }
-}
--- asm-2016-05-26-valhalla-patch/backport-v0.1/resources/EqualatorMain.java
+++ asm-2016-05-26-valhalla-patch/backport-v0.1/resources/EqualatorMain.java
@@ -6,9 +6,13 @@ import java.util.AbstractMap;
 public class EqualatorMain {
 
     public static void main(String[] args) {
+        System.out.println("Here");
         Holder<int> h = new Holder<int>(3);
+        System.out.println("Here");
         Holder<int> h2 = new Holder<int>(3);
+        System.out.println("Here");
         Equalator<int> eq = new Equalator<int>(h, h2);
+        System.out.println("Here");
         System.out.println(Boolean.toString(eq.isEquals()));
         h.f();
     }
